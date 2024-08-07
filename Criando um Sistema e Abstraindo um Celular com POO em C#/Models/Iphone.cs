aa8717628a52c404689d06f9ffaa614bb8603c4d
Vitor N <contato@vitornorton.com.br>
2024-08-07 13:30:37 -0300

    Celular com POO em C#

---
+++ Criando um Sistema e Abstraindo um Celular com POO em C#/Models/Iphone.cs
@@ -0,0 +1,24 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+       
+       
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"O aplicativo: {nomeApp} foi instalado no Iphone {Modelo} pela AppStore" +
+                $" e tem o numero: {Numero} IMEI:{IMEI} com {Memoria}GB de memoria. "); 
+        
+        }
+    }
+}
---
+++ Criando um Sistema e Abstraindo um Celular com POO em C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+
+
+public class Program {
+    public static void Main(String []args)
+    {
+
+        Nokia N = new Nokia("(11)1111-2222", "Nokia G60", "000000-00-000000-0", 128);
+        Iphone I = new Iphone("(11)1111-2222", "Iphone 14", "000000-00-000000-0", 256);
+
+
+        I.Ligar();
+        I.InstalarAplicativo("Whatsapp");  
+        Console.WriteLine(); 
+        N.ReceberLigacao();
+        N.InstalarAplicativo("Telegram");   
+    
+}
+}
