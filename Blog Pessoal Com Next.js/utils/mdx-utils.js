281a6872ade401e2f9e6ac23d9d9667d119a50ac
Vitor N <contato@vitornorton.com.br>
2024-08-22 11:45:15 -0300

    blog next.js

---
+++ Blog Pessoal Com Next.js/utils/mdx-utils.js
@@ -0,0 +1,19 @@
+import { api } from '../services/api'
+
+export const getPosts = async () => {
+    const {data} = await api.get('/posts'); 
+    console.log(data)
+
+    if(data){
+        return data;
+    }
+
+    return []
+}
+
+export const getPostBySlug = async (id) => {
+    const {data} = await api.get(`/posts?id=eq.${id}`)
+    console.log(data);
+
+    return data[0];
+}
